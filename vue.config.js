b0977b03061622567dae95d8d34b9a188d6b1867
yuu-nakagaki <129018884+yuu-nakagaki@users.noreply.github.com>
2023-06-23 13:01:56 +0900

    テスト

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  assetsDir: './'
 })
 
-module.exports = {
-  publicPath: './'
-}
